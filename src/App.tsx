42727c46d18959bfe0bf6dd6c82f9e54f0412655
hiteshkanwar <hitesh.gurjar786@gmail.com>
2023-05-28 23:22:16 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux'
+import { store, persistor } from './Redux/Store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Routes from './Routes';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Provider store={store}>
+			<QueryClientProvider client={queryClient}>
+				<PersistGate loading={null} persistor={persistor}>
+					<BrowserRouter>
+						<Routes/>
+					</BrowserRouter>
+				</PersistGate>
+			</QueryClientProvider>
+		</Provider>
+	);
 }
 
 export default App;
